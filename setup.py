b66167fdd683e70ea6b1b8189f621562039c82b6
Jazib Noel <jazibnoel@gmail.com>
2026-03-21 00:11:22 +0500

    fix issues

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mini-geometry-toolkit",
-    version="0.0.1",
+    version="0.0.2",
     description="A Python toolkit to calculate and visualize geometric shapes",
     author="Your Name",
     author_email="youremail@example.com",
---
+++ src/mini_geometry_toolkit/__init__.py
@@ -0,0 +1,4 @@
+from .circle import area as circle_area, circumference
+from .square import area as square_area, perimeter as square_perimeter
+from .rectangle import area as rect_area, perimeter as rect_perimeter
+from .visualizer import plot_circle, plot_square, plot_rectangle
